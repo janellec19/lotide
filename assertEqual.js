79dd1ef19c028a82a4c16559c7e804f414e9dcb7
Janelle Dube <137248841+janellec19@users.noreply.github.com>
2023-09-19 15:26:37 -0400

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀Assertion Passed: [actual] === [expected]");
+  } else
+    console.log("🚩Assertion Failed: [actual] !== [expected]");
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(20, 1);
