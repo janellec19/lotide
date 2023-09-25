65ff08a528841975a57268811ceb9f7bd04727ef
Janelle Dube <137248841+janellec19@users.noreply.github.com>
2023-09-25 17:23:22 -0400

    updated file

--- tail.js
+++ tail.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("😀Assertion Passed: [actual] === [expected]");
+    console.log(`😀Assertion Passed: ${actual} === ${expected}`);
   } else
-    console.log("🚩Assertion Failed: [actual] !== [expected]");
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const tail = function(array) {
