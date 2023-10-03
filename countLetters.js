68deef44dd5eff2d39046e93cebf60d5c59f1e4b
Janelle Dube <137248841+janellec19@users.noreply.github.com>
2023-10-03 14:59:13 -0400

    countLetters1

--- countLetters.js
+++ countLetters.js
@@ -12,18 +12,20 @@ const countLetters = function(str) {
   const strLowerCase = str.toLowerCase(); //converts str to all lowercase
 
   for (let letters of strLowerCase) { //loops through the string
-
-    if (allLetters[letters] === undefined) { //searches for all letters
-      allLetters[letters] = 1;                //if letter is there, add 1
-    } else {
-      allLetters[letters] ++;                //if letter is there multiple times, add 1 each time
+    if ((/[a-z]/).test(letters)) {
+      if (allLetters[letters] === undefined) { //searches for all letters
+        allLetters[letters] = 1;                //if letter is there, add 1
+      } else {
+        allLetters[letters] ++;                //if letter is there multiple times, add 1 each time
+      }
     }
   }
   return allLetters;
 };
 
 //Test
-let str = ("BRB");
+let str = (" BRB");
 const result1 = countLetters(str, {b: 2, r: 1,});
 assertEqual(result1["b"], 2);
 assertEqual(result1["r"], 1);
+assertEqual(result1[" "], undefined);
