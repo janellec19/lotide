cec3c0d0d24827f40e1e90f29038ec811e0cf9a5
Janelle Dube <137248841+janellec19@users.noreply.github.com>
2023-10-07 16:56:06 -0400

    findKey

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {        //scan the object using object.keys method
+    const target = obj[key];
+    const comparisonResult = callback(target);
+    if (comparisonResult) {
+      return key;
+    }
+  }
+};
+
+const stars = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+assertEqual(findKey(stars, x => x.stars === 2), "noma"); // => "noma"
+assertEqual(findKey(stars, x => x.stars === 1), "Blue Hill"); // => "Blue Hill"
